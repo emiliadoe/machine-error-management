835506a6028efdb63fb48b35a2de5c3b13872120
emiliadoe <emilia.d@web.de>
2024-10-04 13:29:04 +0300

    feature: errorCode Model added

--- errorManagementSystem/djangoApp/admin.py
+++ errorManagementSystem/djangoApp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Machine
+from .models import Machine, ErrorCode
 
 # Register your models here.
 
 admin.site.register(Machine)
+
+admin.site.register(ErrorCode)
--- errorManagementSystem/djangoApp/models.py
+++ errorManagementSystem/djangoApp/models.py
@@ -15,4 +15,18 @@ class Machine(models.Model):
 
     def __str__(self):
         return self.name
+    
+class ErrorCode(models.Model):
+
+    class Meta: 
+        verbose_name = 'ErrorCode'
+        verbose_name_plural = 'ErrorCodes'
+
+    error_code = models.CharField(max_length=20)
+
+    error_description = models.CharField(max_length=400)
+
+    solution = models.CharField(max_length=400)
+
+    machine = models.ForeignKey(Machine, on_delete=models.CASCADE) 
 
