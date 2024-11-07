e61f129799e9e595e7a49d6a5b79206280803b62
emiliadoe <emilia.d@web.de>
2024-11-08 01:04:00 +0300

    protocol added and menu design changed

--- errorManagementSystem/djangoApp/context_processor.py
+++ errorManagementSystem/djangoApp/context_processor.py
@@ -1,10 +1,6 @@
 from .models import ErrorProtocol
 
 def global_data(request):
-    """
-    This function returns context data that will be available in every template.
-    In this case, it's the list of all machines and error protocols.
-    """
     
     protocols = ErrorProtocol.objects.all()  
 
