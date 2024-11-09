770048a71f5c2e2065750431717ac591664ca51a
emiliadoe <emilia.d@web.de>
2024-11-09 20:28:23 +0300

    added all data and changes menu+

--- errorManagementSystem/errorManagementSystem/settings.py
+++ errorManagementSystem/errorManagementSystem/settings.py
@@ -83,16 +83,22 @@ WSGI_APPLICATION = 'errorManagementSystem.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME':  'manage-errors',
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'PORT': '5432',
-        'HOST': 'localhost'
-    }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME':  'manage-errors',
+         'USER': os.getenv('DB_USER'),
+         'PASSWORD': os.getenv('DB_PASSWORD'),
+         'PORT': '5432',
+         'HOST': 'localhost'
+     }
+ }
 
+#DATABASES = {
+#    'default': {
+#  'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
