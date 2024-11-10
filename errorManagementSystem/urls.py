c7442de324d655cc925955f53e2c6136f976973d
emiliadoe <emilia.d@web.de>
2024-11-10 23:45:53 +0300

    static paths changed

--- errorManagementSystem/urls.py
+++ errorManagementSystem/urls.py
@@ -35,6 +35,3 @@ urlpatterns += i18n_patterns(
 )
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
