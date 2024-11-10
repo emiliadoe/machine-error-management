1c0daaf08106bc6a2c2e49a1a4120fe888bfee66
emiliadoe <emilia.d@web.de>
2024-11-10 23:15:16 +0300

    added trusted CSRF

--- errorManagementSystem/settings.py
+++ errorManagementSystem/settings.py
@@ -81,6 +81,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'errorManagementSystem.wsgi.application'
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://machine-error-management.onrender.com',
+]
+
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
