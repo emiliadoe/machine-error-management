55713f93e834f8f9e917fdabc78f6323cd23f5a4
emiliadoe <emilia.d@web.de>
2024-11-10 20:01:48 +0300

    static files updated

--- errorManagementSystem/urls.py
+++ errorManagementSystem/urls.py
@@ -33,6 +33,8 @@ urlpatterns += i18n_patterns(
     path('', include('djangoApp.urls'), name='home'),
     path('i18n/setlang/', set_language, name='set_language'),
 )
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
