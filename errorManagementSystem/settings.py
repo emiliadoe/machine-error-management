aa7da7fa0f0b8f2c1f9544466310b6d9410411c2
emiliadoe <emilia.d@web.de>
2024-11-10 23:02:37 +0300

    db updated

--- errorManagementSystem/settings.py
+++ errorManagementSystem/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g=@v3$!hzdivkx91o4s81h@#xuor9$@u4*uu*ay14dbaejpfko'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['machine-error-management.onrender.com', '127.0.0.1', '0.0.0.0']
 """ 
