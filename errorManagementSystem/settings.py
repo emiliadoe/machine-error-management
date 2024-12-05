ff0bd2868c0f6333d60a30ba1954c82bd40b5d5d
emiliadoe <emilia.d@web.de>
2024-12-05 17:49:18 +0300

    debug settings chagnges

--- errorManagementSystem/settings.py
+++ errorManagementSystem/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g=@v3$!hzdivkx91o4s81h@#xuor9$@u4*uu*ay14dbaejpfko'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 """ 
 DEBUG = False
 """
